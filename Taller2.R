faf4932ae859b7ce6b072ec48cbc62d827495a1f
Julián Camargo <ja.camargof@uniandes.edu.co>
2022-09-29 17:33:00 -0500

    Update Taller2.R

--- Taller2.R
+++ Taller2.R
@@ -36,16 +36,8 @@ table(train_hogares$Pobre_hand)
 table(train_hogares$Pobre,train_hogares$Pobre_hand)
 train_hogares$Indigente
 
-#####Limpieza Bases#####
+####Emparejar Bases####
 
-# paquetes
-require("here")
-require("tidyverse")
-#cargar bases
-test_hogares <- readRDS("C:/Users/Luz Myriam Fonseca/Documentos/Julian/9 Semestre/BDMLAE/Taller 2/data/test_hogares.Rds")
-test_personas <- readRDS("C:/Users/Luz Myriam Fonseca/Documentos/Julian/9 Semestre/BDMLAE/Taller 2/data/test_personas.Rds")
-train_hogares <- readRDS("C:/Users/Luz Myriam Fonseca/Documentos/Julian/9 Semestre/BDMLAE/Taller 2/data/train_hogares.Rds")
-train_personas <- readRDS("C:/Users/Luz Myriam Fonseca/Documentos/Julian/9 Semestre/BDMLAE/Taller 2/data/train_personas.Rds")
 
 ####train_hogares####
 
@@ -53,13 +45,23 @@ train_personas <- readRDS("C:/Users/Luz Myriam Fonseca/Documentos/Julian/9 Semes
 glimpse(train_hogares)
 glimpse(test_hogares)
 
+#Vamos a equilibrar train y test para no usar variables de train que no
+#están en test.
+#Pobre y Ingpcug se quedan en train porque son nuestras variables de interés
+#Se quitan Ingtotug, Ingtotugarr, Indigente, Npobres, Nindigentes
+train_hogares <- select(train_hogares, -c(Ingtotug, Ingtotugarr, Indigente, Npobres, Nindigentes))
+colnames(train_hogares)
+colnames(test_hogares)
+
 #NAs: P5100, p5130, p5140 tienen NAs
 sapply(train_hogares, function(x) sum(is.na(x)))
 #Estas variables parecen no ser relevantes para predecir el ingreso
 #ya que hablan de los pagos por amortización, el valor estimado del arriendo
 #de sus viviendas y el pago mensual por arriendo.
 #podrían dropearse
-train_hogares <- select(train_hogares, -c(P5100, P5130, P5140, Fex_c, Fex_dpto))
+#También dropeamos Dominio, ya que para controlar por la ubicación se tiene
+#Depto y Cabecera
+train_hogares <- select(train_hogares, -c(Dominio, P5100, P5130, P5140, Fex_c, Fex_dpto))
 
 # Arreglamos la variable Clase (dicótoma) para que sean 1 y 0
 #tabulación de Clase
@@ -78,7 +80,7 @@ variables_dicotomas <- c("Clase")
 train_hogares[,variables_dicotomas] = train_hogares[,variables_dicotomas] - 1
 
 #Definimos variables categoricas
-variables_categoricas <- c("Dominio","P5090", "Depto")
+variables_categoricas <- c("P5090", "Depto")
 for (v in variables_categoricas) {
   train_hogares[, v] <- as.factor(train_hogares[, v, drop = T])
 }
@@ -87,12 +89,9 @@ for (v in variables_categoricas) {
 
 #convertimos id en string
 
-tr_hog_d <- model.matrix(~ Clase + Dominio +
-                           P5000 + P5010 + P5090 + Nper + 
-                           Npersug + Ingtotug + 
-                           Ingtotugarr + Ingpcug + Li + 
-                           Lp + Pobre + Indigente + Npobres
-                         + Nindigentes + Depto, train_hogares) %>%
+tr_hog_d <- model.matrix(~ Clase + P5000 + P5010 + P5090 + Nper + 
+                           Npersug + Ingpcug + Li + 
+                           Lp + Pobre + Depto, train_hogares) %>%
   as.data.frame()
 
 colnames(tr_hog_d)
@@ -113,7 +112,7 @@ sapply(test_hogares, function(x) sum(is.na(x)))
 #ya que hablan de los pagos por amortización, el valor estimado del arriendo
 #de sus viviendas y el pago mensual por arriendo.
 #podrían dropearse
-test_hogares <- select(test_hogares, -c(P5100, P5130, P5140, Fex_c, Fex_dpto))
+test_hogares <- select(test_hogares, -c(Dominio, P5100, P5130, P5140, Fex_c, Fex_dpto))
 
 # Arreglamos la variable Clase (dicótoma) para que sean 1 y 0
 #tabulación de Clase
@@ -132,86 +131,20 @@ variables_dicotomas <- c("Clase")
 test_hogares[,variables_dicotomas] = test_hogares[,variables_dicotomas] - 1
 
 #Definimos variables categoricas
-variables_categoricas <- c("Dominio","P5090", "Depto")
+variables_categoricas <- c("P5090", "Depto")
 for (v in variables_categoricas) {
   test_hogares[, v] <- as.factor(test_hogares[, v, drop = T])
 }
 
 #Dumificamos la base
 
-te_hog_d <- model.matrix(~ Clase + Dominio +
-                           P5000 + P5010 + P5090 + Nper + 
-                           Npersug + Li + 
-                           Lp + Depto, train_hogares) %>%
+te_hog_d <- model.matrix(~ Clase + P5000 + P5010 + P5090 + Nper + 
+                           Npersug + Li + Lp + Depto, train_hogares) %>%
   as.data.frame()
 
 colnames(te_hog_d)
-
+colnames(tr_hog_d)
 glimpse(te_hog_d)
-
-####Lasso####
-library(pacman)
-p_load(fastDummies, caret, glmnet, MLmetrics)
-
-# Convertimos la y en log (y1->Pobre , y2->Lp)
-#y1 -> modelo con dummy
-y1_train <- tr_hog_d[,"Pobre"]
-#y2 -> modelo con linea de pobreza
-y2_train <- log(tr_hog_d[,"Ingpcug"])
-#Vector de X
-X_train <- select(tr_hog_d, -c(Pobre, Ingpcug))
-#no tengo variable "pobre" en test
-#y1_test <- te_hog_d[,","Pobre"]
-#y2 -> modelo con ingreso per capita, no la tengo en test
-#y2_test <- log(tr_hog_d[,"Ingpcug"])
-#Vector  de X test
-X_test <- te_hog_d
-
-# Estandarizamos la base de datos. 
-# Necesitamos estandarizar
-#####Estandarización#####
-mu <- mean(X_train$P5000)
-sigma <- sd(X_train$P5000)
-X_train$P5000 <- (X_train$P5000 - mu)/sigma
-X_test$P5000 <- (X_test$P5000 - mu)/sigma
-
-mu <- mean(X_train$P5010)
-sigma <- sd(X_train$P5010)
-X_train$P5010 <- (X_train$P5010 - mu)/sigma
-X_test$P5010 <- (X_test$P5010 - mu)/sigma
-
-mu <- mean(X_train$Nper)
-sigma <- sd(X_train$Nper)
-X_train$Nper <- (X_train$Nper - mu)/sigma
-X_test$Nper <- (X_test$Nper - mu)/sigma
-
-mu <- mean(X_train$Npersug)
-sigma <- sd(X_train$Npersug)
-X_train$Npersug <- (X_train$Npersug - mu)/sigma
-X_test$Npersug <- (X_test$Npersug - mu)/sigma
-
-mu <- mean(X_train$Ingtotug)
-sigma <- sd(X_train$Ingtotug)
-X_train$Ingtotug <- (X_train$Ingtotug - mu)/sigma
-
-mu <- mean(X_train$Ingtotugarr)
-sigma <- sd(X_train$Ingtotugarr)
-X_train$Ingtotugarr <- (X_train$Ingtotugarr - mu)/sigma
-
-mu <- mean(X_train$Li)
-sigma <- sd(X_train$Li)
-X_train$Li <- (X_train$Li - mu)/sigma
-X_test$Li <- (X_test$Li - mu)/sigma
-
-mu <- mean(X_train$Npobres)
-sigma <- sd(X_train$Npobres)
-X_train$Npobres <- (X_train$Npobres - mu)/sigma
-
-mu <- mean(X_train$Nindigentes)
-sigma <- sd(X_train$Nindigentes)
-X_train$Nindigentes <- (X_train$Nindigentes - mu)/sigma
-
-glimpse(X_train)
-glimpse(X_test)
+glimpse(tr_hog_d)
 
 
